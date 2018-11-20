922fcb1d4f77ae314e84ba63e1fbd8a3046cdc92
gregfoulkes <greg_foulkes@live.com>
2018-11-20 15:05:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+//const http = require('http').Server(app);
+//const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('express-flash');
+
+//database config
+const Config = require('./config/database_connection')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(session({
+  secret: '@pp Factori3',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(flash());
+
+app.get('/', function(req,res) {
+
+  res.send('Lend Me')
+
+} );
+
+var port = process.env.port || 3007;
+app.listen(port, function(){
+    console.log('running at port :' , port)
+});
