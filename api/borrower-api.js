a14db6fd3576ad2b0ca19a880f0f4a6da3a799aa
Busisile-Zilwa2601 <busisile.box@gmail.com>
2018-11-20 17:23:49 +0200

    added id and id_addPay

--- api/borrower-api.js
+++ api/borrower-api.js
@@ -6,11 +6,13 @@ module.exports = function borrowerApiCall(){
         let username = req.params.borrowerName;
         res.status(200).json({
             message: 'Handling GET request returning the borrower information',
+            id: 1,
+            id_addPay: 000110011,
             firstName: username,
             lastName: "xxxx",
             email: "xxx@xxx.xxx",
             mobile: 0000000000,
-            role: "borrowe"
+            role: "borrower"
         });
     });
 
